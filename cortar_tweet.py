988a096aa26097858c4aa342d01fcaaede2752f3
MarkelJM <markjuaristi@gmail.com>
2022-11-02 20:09:41 +0100

    texto cortado  & añadido a lista

---
+++ cortar_tweet.py
@@ -0,0 +1,17 @@
+
+
+class CutTweets():
+
+    def __init__(self) -> None:
+        pass
+
+    def read_all_text(self, text, list):
+        tweet = []
+        for pos in range(len(list)):
+            lim_low = list[pos]
+            lim_up = list[pos +1 ]
+            tweet.append(self.select_text(text, lim_low, lim_up))
+        return tweet
+            
+    def select_text(self, text, lim1, lim2):
+        return text[ lim1 : lim2 ]
